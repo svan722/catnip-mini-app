6da76bd0895626f890941a8b363dcf57d6b7cbe8
supercoolx <94512319+supercoolx@users.noreply.github.com>
2024-10-03 14:49:48 -0700

    change x links

--- frontend/src/libs/constants.js
+++ frontend/src/libs/constants.js
@@ -7,10 +7,10 @@ export const LINK = {
     TELEGRAM_MINIAPP: 'https://t.me/supercool_catnip_bot',
     TELEGRAM_GROUP: 'https://t.me/tonredonion_onon',
     TELEGRAM_CHANNEL: 'https://t.me/CATNIP_ANNOUNCEMENT',
-    X: 'https://x.com/TONONION_ONON?t=WKRiaAMa4ZPsfzPpj-g_sg&s=09',
+    X: 'https://x.com/catnipsprint',
     YOUTUBE: 'https://youtube.com/@Cointswap',
     WEBSITE: 'https://coint.xyz/',
-    TWEET: 'https://x.com/TONONION_ONON/status/1840404476878037043?t=xsTWy9nkhQuXk95VrHCAHQ&s=19',
+    TWEET: 'https://x.com/catnipsprint',
     MANIFEST: 'https://supercoolx.github.io/manifests/catnip-manifest.json',
 }
 
